6de7d42610f55fea725b7482f8d4029be1f6fc3a
Mikhail Ivanov <m.ivanych@gmail.com>
2019-08-19 10:48:56 +0300

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Telemetry::Any - It's new $module
+
+# SYNOPSIS
+
+    use Telemetry::Any;
+
+# DESCRIPTION
+
+Telemetry::Any is ...
+
+# LICENSE
+
+Copyright (C) Mikhail Ivanov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Mikhail Ivanov <m.ivanych@gmail.com>
---
+++ lib/Telemetry/Any.pm
@@ -0,0 +1,39 @@
+package Telemetry::Any;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Telemetry::Any - It's new $module
+
+=head1 SYNOPSIS
+
+    use Telemetry::Any;
+
+=head1 DESCRIPTION
+
+Telemetry::Any is ...
+
+=head1 LICENSE
+
+Copyright (C) Mikhail Ivanov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Mikhail Ivanov E<lt>m.ivanych@gmail.comE<gt>
+
+=cut
+
