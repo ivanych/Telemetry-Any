df4f6f42062f620c9fb97b07b8f940e2d97cbf95
Mikhail Ivanov <m.ivanych@gmail.com>
2019-08-24 00:46:30 +0300

    The report() method now accepts the format parameter

--- lib/Telemetry/Any.pm
+++ lib/Telemetry/Any.pm
@@ -33,7 +33,70 @@ sub import {
     return;
 }
 
-sub reset {              ## no critic (Subroutines::ProhibitBuiltinHomonyms)
+sub report {
+    my ( $self, %args ) = @_;
+
+    ## calculate total time (start time vs last time)
+
+    my $total_time = Time::HiRes::tv_interval( $self->{times}->[0], $self->{times}->[ $self->{count} - 1 ] );
+
+    if ( $args{format} eq 'table' ) {
+        $self->print( ref($self) . ' Report -- Total time: ' . sprintf( '%.4f', $total_time ) . ' secs' );
+    }
+
+    if ( $args{collapse} ) {
+        $self->_calculate_collapsed;
+
+        if ( $args{format} eq 'table' ) {
+            $self->print('Count     Time    Percent');
+            $self->print('----------------------------------------------');
+        }
+
+        my $c       = $self->{collapsed};
+        my $sort_by = $args{sort_by} || 'time';
+        my @labels  = sort { $c->{$b}->{$sort_by} <=> $c->{$a}->{$sort_by} } keys %$c;
+        foreach my $label (@labels) {
+            my $count = $c->{$label}->{count};
+            my $time  = $c->{$label}->{time};
+            my $msg = sprintf( '%8s  %.4f  %5.2f%%  %s', ( $count, $time, ( ( $time / $total_time ) * 100 ), $label ) );
+            $self->print($msg);
+        }
+        return 1;
+    }
+
+    if ( $args{format} eq 'table' ) {
+        $self->print('Interval  Time    Percent');
+        $self->print('----------------------------------------------');
+    }
+
+    ## sort interval structure based on value
+
+    @{ $self->{intervals} } = sort { $b->{value} <=> $a->{value} } @{ $self->{intervals} };
+
+    ##
+    ## report of each time space between marks
+    ##
+
+    for my $i ( @{ $self->{intervals} } ) {
+        ## skip first time (to make an interval,
+        ## compare the current time with the previous one)
+
+        next if ( $i->{index} == 0 );
+
+        my $msg = sprintf(
+            '%02d -> %02d  %.4f  %5.2f%%  %s -> %s',
+            ( $i->{index} - 1 ),
+            $i->{index}, $i->{value},    #$self->print("\n");
+            ( ( $i->{value} / $total_time ) * 100 ),
+            $self->{label}->{ ( $i->{index} - 1 ) },
+            $self->{label}->{ $i->{index} }
+        );
+
+        $self->print($msg);
+    }
+}
+
+sub reset {                              ## no critic (Subroutines::ProhibitBuiltinHomonyms)
     my ($self) = @_;
 
     %{$self} = (
